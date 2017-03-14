6f32d33ab63a2ddee87586ab0e22cfdda5c0f92b
Rafal Franiewski <194944@edu.p.lodz.pl>
2017-03-14 05:00:21 +0100

    Extracted methods from StacksDemo & little refactor

--- src/test/java/edu/kis/vh/stacks/demo/StacksDemo.java
+++ src/test/java/edu/kis/vh/stacks/demo/StacksDemo.java
@@ -8,27 +8,39 @@ class StacksDemo {
 
     public static final int ITERATOR = 15;
 
+
     public static void main(String[] args) {
         DefaultStacksFactory factory = new DefaultStacksFactory();
 
+        testStacks(factory);
+    }
+    private static void testStacks(DefaultStacksFactory factory) {
         Stack[] Stacks = {factory.getStandardStack(), factory.getFalseStack(), factory.getFIFOStack(),
                 factory.getHanoiStack()}; //przeniesienie kodu do nowej linii w momencie, gdy nie jest to potrzebne
 
+        System.out.println("Rejected: " + ((StackHanoi) Stacks[3]).reportRejected());
+
+    }
+
+    private static void fillStacks(Stack[] stacks) {
         for (int i = 1; i < 15; i++)
             for (int j = 0; j < 3; j++)
-                Stacks[j].push(i);
+                stacks[j].push(i);
+    }
+
+    private static void fillHanoiStack(Stack stack) {
 
         java.util.Random rn = new java.util.Random(); //cofniecie wiersza
         for (int i = 1; i < ITERATOR; i++)
-            Stacks[3].push(rn.nextInt(20));
+            stack.push(rn.nextInt(20));
+    }
 
-        for (int i = 0; i < Stacks.length; i++) {       //cofniecie wiersza
-            while (!Stacks[i].isEmpty())
-                System.out.print(Stacks[i].pop() + "  ");
+    private static void printStack(Stack[] stacks) {
+        for (int i = 0; i < stacks.length; i++) {       //cofniecie wiersza
+            while (!stacks[i].isEmpty())
+                System.out.print(stacks[i].pop() + "  ");
             System.out.println(); //while odnosi sie tylko do 1 linii po sobie, wiec kolejna linia, nie odnosi sie
         }
-
-        System.out.println("total rejected is " + ((StackHanoi) Stacks[3]).reportRejected());
 
     }
 //alt + <- w srodowisku IntelliJ Idea powoduje przelaczanie sie pomiedzy kartami
