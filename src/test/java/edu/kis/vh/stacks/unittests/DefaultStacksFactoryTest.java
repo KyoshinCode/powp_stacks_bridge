4fbe54a7b23cc5dc3b765fb4dbdd3bbbdce00986
Rafal Franiewski <194944@edu.p.lodz.pl>
2017-03-14 05:10:23 +0100

    Added DefaultStacksFactoryTest

---
+++ src/test/java/edu/kis/vh/stacks/unittests/DefaultStacksFactoryTest.java
@@ -0,0 +1,38 @@
+package edu.kis.vh.stacks.unittests;
+
+import edu.kis.vh.stacks.Stack;
+import edu.kis.vh.stacks.StackFIFO;
+import edu.kis.vh.stacks.StackHanoi;
+import edu.kis.vh.stacks.factory.DefaultStacksFactory;
+import junit.framework.Assert;
+import org.junit.Test;
+
+
+/**
+ * Created by RFran on 14.03.2017.
+ */
+public class DefaultStacksFactoryTest {
+    @Test public void testStandardStackFactory(){
+        DefaultStacksFactory factory = new DefaultStacksFactory();
+        Stack result = factory.getStandardStack();
+        Assert.assertTrue(result instanceof Stack);
+    }
+
+    @Test public void testFalseStackFactory(){
+        DefaultStacksFactory factory = new DefaultStacksFactory();
+        Stack result = factory.getFalseStack();
+        Assert.assertTrue(result instanceof Stack);
+    }
+
+    @Test public void testFIFOStackFactory(){
+        DefaultStacksFactory factory = new DefaultStacksFactory();
+        Stack result = factory.getFIFOStack();
+        Assert.assertTrue(result instanceof StackFIFO);
+    }
+
+    @Test public void testHanoiStackFactory(){
+        DefaultStacksFactory factory = new DefaultStacksFactory();
+        Stack result = factory.getHanoiStack();
+        Assert.assertTrue(result instanceof StackHanoi);
+    }
+}
