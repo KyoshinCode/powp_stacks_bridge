fe5478f9660a22707809d41010148e765cd95513
Rafal Franiewski <194944@edu.p.lodz.pl>
2017-03-14 03:58:46 +0100

    Refactor more names. Changed access modificators

--- src/main/java/edu/kis/vh/stacks/Stack.java
+++ src/main/java/edu/kis/vh/stacks/Stack.java
@@ -2,34 +2,37 @@ package edu.kis.vh.stacks;
 
 public class Stack {
 
-    public int total = -1;
-    public static final int NEGATIVE = -1;
-    public static final int POSITIVE = 11;
-    private int[] ITEMS = new int[12];
+    private int total = EMPTY_STACK_INDICATOR;
+    
+    private static final int EMPTY_STACK_INDICATOR = -1;
+    private static final int FULL_STACK_INDICATOR = 11;
+    
+    
+    private int[] items = new int[12];
 
     public void push(int i) {
         if (!isFull())
-            ITEMS[++total] = i;
+            items[++total] = i;
     }
 
     public boolean isEmpty() {
-        return total == NEGATIVE;
+        return total == EMPTY_STACK_INDICATOR;
     }
 
     public boolean isFull() {
-        return total == POSITIVE;
+        return total == FULL_STACK_INDICATOR;
     }
 
     public int top() {
         if (isEmpty())
-            return NEGATIVE;
-        return ITEMS[total];
+            return EMPTY_STACK_INDICATOR;
+        return items[total];
     }
 
     public int pop() {
         if (isEmpty())
-            return NEGATIVE;
-        return ITEMS[total--];
+            return EMPTY_STACK_INDICATOR;
+        return items[total--];
     }
 
 }
